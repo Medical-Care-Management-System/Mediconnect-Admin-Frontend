c4cf9b1ffc55e498fd4ce533db275bb220e2e76f
Tharushika M.T. <thilinitharushika.21@cse.mrt.ac.lk>
2024-09-29 19:28:52 +0530

    Manage Account

--- src/ManageAccount.jsx
+++ src/ManageAccount.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ManageAccount.css';
 
 const ManageAccount = () => {
+  const [selectedOption, setSelectedOption] = useState('NIC');
+  const [passwordInputs, setPasswordInputs] = useState({
+    currentPassword: '',
+    newPassword: '',
+    confirmPassword: '',
+  });
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const handlePasswordInputChange = (event) => {
+    const { name, value } = event.target;
+    setPasswordInputs({
+      ...passwordInputs,
+      [name]: value,
+    });
+  };
+
   return (
     <div className="manage-accounts">
       <div className="deactivate-account">
@@ -12,15 +31,45 @@ const ManageAccount = () => {
 
       <div className="edit-account">
         <label>Edit Accounts</label>
-        <input type="text" value="emilywhite@gmail.com" readOnly/>
-        <div className='change'>      
-        <select >
-          <option value="NIC">Change NIC</option>
-          <option value="UserName">Change User Name</option>
-          <option value="Password">Change Password</option>
-        </select>
-        <input type="text" />
-        <button className="save-btn">Save</button></div>
+        <input type="text" value="emilywhite@gmail.com" readOnly />
+        <div className="change">
+          <select value={selectedOption} onChange={handleOptionChange}>
+            <option value="NIC">Change NIC</option>
+            <option value="UserName">Change User Name</option>
+            <option value="Password">Change Password</option>
+          </select>
+
+          {/* Conditionally render password inputs within the same div */}
+          {selectedOption === 'Password' ? (
+            <>
+              <input
+                type="password"
+                name="currentPassword"
+                placeholder="Current Password"
+                value={passwordInputs.currentPassword}
+                onChange={handlePasswordInputChange}
+              />
+              <input
+                type="password"
+                name="newPassword"
+                placeholder="New Password"
+                value={passwordInputs.newPassword}
+                onChange={handlePasswordInputChange}
+              />
+              <input
+                type="password"
+                name="confirmPassword"
+                placeholder="Confirm New Password"
+                value={passwordInputs.confirmPassword}
+                onChange={handlePasswordInputChange}
+              />
+            </>
+          ) : (
+            <input type="text" placeholder={selectedOption} />
+          )}
+
+          <button className="save-btn">Save</button>
+        </div>
       </div>
 
       <div className="suspicious-activity-alerts">
